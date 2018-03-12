631d70b29595e63e472acdd2d4a0d64b0a7b46c2
I-Mircheva <ivona1mircheva@gmail.com>
2018-03-12 21:54:16 +0200

    don't forget your routes

--- Pile/config/routes.rb
+++ Pile/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :schools
   resources :students
+  get '/B_10_Ivona_Mircheva_schools/', to: 'schools#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
